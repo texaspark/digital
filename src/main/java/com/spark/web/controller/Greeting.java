adffd549062f0271393ebc3932cd0d372da4410c
seokpark <seokpark@SEOKPARK-MY.kr.oracle.com>
2021-03-02 21:02:57 +0800

    202103022101

--- src/main/java/com/spark/web/controller/Greeting.java
+++ src/main/java/com/spark/web/controller/Greeting.java
@@ -1,14 +1,16 @@
 package com.spark.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
     @Controller
     public class Greeting{
 
-        @RequestMapping(value="/home")
-        public String home() throws Exception{
-            return "index";
+        @RequestMapping(value="/home/{name}/{address}/{hobby}")
+        public String home(@PathVariable String name, @PathVariable String address,
+                           @PathVariable String hobby) throws Exception{
+            return "home";
         }
         @RequestMapping(value="/archive")
         public String archive() throws Exception{
